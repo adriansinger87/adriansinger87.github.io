670ddce8b2e08c73fd2c987a1eed97bc3dfecedf
adrian <adriansinger87@gmail.com>
2019-07-19 23:38:32 +0200

    banner and animation added

--- assets/js/init.js
+++ assets/js/init.js
@@ -5,5 +5,20 @@
     $('.sidenav').sidenav();
     $('.modal').modal();
 
+    InitImage();
+
   }); // end of document ready
+
+  function InitImage() {
+    var loc = window.location.href;
+    $("#chemnitz-banner").attr("src", loc + "/assets/img/banner_" + Random(1,4) + ".jpg");
+
+    function Random(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+  }
+ 
+
 })(jQuery); // end of jQuery name space
