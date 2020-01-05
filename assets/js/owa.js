386865d5528f511d1984299f78018e3a331a05ed
adrian <adriansinger87@gmail.com>
2020-01-05 14:58:10 +0100

    owa test integrated

---
+++ assets/js/owa.js
@@ -0,0 +1,8 @@
+// Set base URL
+OWA.setSetting('baseUrl', 'http://owa.wargaz-vikings.de/');
+// Create a tracker
+OWATracker = new OWA.tracker();
+OWATracker.setSiteId('24be8aff0d5864ddd8bc4ca880f7e972');
+OWATracker.trackPageView();
+OWATracker.trackClicks();
+OWATracker.trackDomStream();
